49a367c84d0686ca8128c50725a1f801154398c7
Jonathan Kempf <kempfjj@gmail.com>
2022-08-27 09:44:59 -0400

    Initial commit for ICO

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x4547599c4a684a880334f52D0b00BF2FFa1F7fDE";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
